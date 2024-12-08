a78b8cb44898bb2530d2893134c05f48fada956a
PhilippMerunka <dukezero@outlook.de>
2024-12-08 18:05:03 +0100

    Frontend improvement (sorting, filtering). Categories added into admin interface

--- products/views.py
+++ products/views.py
@@ -17,5 +17,23 @@ class ProductViewSet(ModelViewSet):
 def product_overview(request):
     if not request.user.is_authenticated:
         return redirect('login')
-    products = Product.objects.all()  # Fetch all products
-    return render(request, 'products/products.html', {'products': products})
+    
+    products = Product.objects.all()
+    all_categories = Category.objects.all()
+
+    # Filtering by category
+    category_filter = request.GET.get('category')
+    if category_filter:
+        products = products.filter(categories__id=category_filter)
+    
+    # Sorting
+    sort = request.GET.get('sort')
+    order = request.GET.get('order', 'asc')
+    if sort in ['name', 'price']:
+        sort_key = sort if order == 'asc' else f'-{sort}'
+        products = products.order_by(sort_key)
+    
+    return render(request, 'products/products.html', {
+        'products': products,
+        'all_categories': all_categories,
+    })
